45a175eedfb3c6cabf101f3edc5ea443c3682465
Bl1nker <Alex_001@sibmail.com>
2024-09-25 17:04:20 +0700

    Task 4 done. All tasks done

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static int[] RemoveNegatives(int[] numbers)
+    {
+        int[] res = numbers.Where(x => x >= 0).ToArray();
+        return res;
+    }
+    static void Main(string[] args)
+    {
+        int[] numbers = { 5, -3, 9, -1, 2, -7, 0 }; // Пример массива
+        int[] result = RemoveNegatives(numbers);
+        Console.WriteLine("[" + string.Join(", ", result) + "]");
+    }
+}
