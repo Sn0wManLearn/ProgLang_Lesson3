f9672768079f1fa3513398ff1ad276e5d1b27af7
Bl1nker <Alex_001@sibmail.com>
2024-09-25 16:47:51 +0700

    Task 1 done

---
+++ Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static bool IsNumberPresent(int[] numbers, int numberToFind)
+    {
+        if (numbers.Contains(numberToFind)) return true;
+        // foreach(int number in numbers){
+        //     if(number == numberToFind) return true;
+        // }
+        return false;
+    }
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 3, 4, 19, 3 }; // Пример массива
+        int numberToFind = 8; // Пример числа для поиска
+        if (IsNumberPresent(numbers, numberToFind))
+        {
+            Console.WriteLine("Присутствует");
+        }
+        else
+        {
+            Console.WriteLine("Не присутствует");
+        }
+    }
+}
