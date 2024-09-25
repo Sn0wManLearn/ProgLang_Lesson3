a30096344726c4eebe0f7a61938b11300f1e8514
Bl1nker <Alex_001@sibmail.com>
2024-09-25 16:54:51 +0700

    Task 2 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void SwapSigns(int[] numbers)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = -numbers[i];
+        }        
+    }
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, -5, 6, -7, 3, 7, -2, 8, 10, -9 }; // Пример массива
+        SwapSigns(numbers);
+        Console.WriteLine("[" + string.Join(", ", numbers) + "]");
+    }
+}
