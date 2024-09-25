96c806838949ddb90eb9ab171bca5149ca23ea14
Bl1nker <Alex_001@sibmail.com>
2024-09-25 16:59:30 +0700

    Task 3 done

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static double CalculateAverage(int[] numbers)
+    {
+        int sum = numbers.Sum();
+        double res = (double) sum / numbers.Length;
+        return res;
+    }
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Пример массива
+        double average = CalculateAverage(numbers);
+        Console.WriteLine(average);
+    }
+}
